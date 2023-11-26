74af6dd6d6ac9c9505d35538c4dc89e899654722
baris5534 <samsuncrsmb55@gmail.com>
2023-11-26 17:47:26 +0300

    ddd

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -7,8 +7,8 @@ import TheWelcome from './components/TheWelcome.vue'
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="100" height="100" />
 
-    <div class="wrapper">
-      <HelloWorld class="yazi bg-slate-400" msg="Benim uygulamam!" />
+    <div class="wrapper bg-slate-300">
+      <HelloWorld class="yazi bg-slate-400 text-white" msg="Bizim uygulamamız" />
     </div>
   </header>
 
@@ -16,34 +16,3 @@ import TheWelcome from './components/TheWelcome.vue'
   </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-    color: indigo;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-  .yazi {
-    color: indigo;
-  }
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
