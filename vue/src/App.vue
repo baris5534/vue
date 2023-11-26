592f4c48ed64c7095484e83b2a253563ff341958
baris5534 <samsuncrsmb55@gmail.com>
2023-11-26 17:14:40 +0300

    first commit

---
+++ vue/src/App.vue
@@ -0,0 +1,51 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import Navbar from './components/navbar.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="100" height="100" />
+
+    <div class="wrapper">
+      <HelloWorld class="yazi" msg="Benim uygulamam!" />
+      <navbar id="navbar"/>
+    </div>
+  </header>
+
+  <main>
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+    color: indigo;
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+  .yazi {
+    color: indigo;
+  }
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ vue/src/components/navbar.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <span>{{merhabalar navbar}}</span>
+    </div>
+</template>
+
+<script setup>
+    new Vue({
+        el:"#navbar",
+        data:{
+            title:"navbar kısmı burası",
+        }
+    });
+</script>
+
+<style lang="scss" scoped>
+
+</style>
